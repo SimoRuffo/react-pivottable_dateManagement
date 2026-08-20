082c9d33c27e8d53f0d5364b005236e905f77bb5
Simone Sante Ruffo <31985927+SimoRuffo@users.noreply.github.com>
2026-08-20 16:47:09 +0200

    Update PivotTableUI.jsx

--- src/PivotTableUI.jsx
+++ src/PivotTableUI.jsx
@@ -12,7 +12,7 @@ export class DraggableAttribute extends React.Component {
   constructor(props) {
     super(props);
     this.state = {open: false, filterText: '',filterTextFrom:'',filterTextTo:''};
-
+  }
 	
   toggleValue(value) {
     if (value in this.props.valueFilter) {
